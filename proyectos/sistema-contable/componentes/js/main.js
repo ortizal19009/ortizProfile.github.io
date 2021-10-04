60460130be3d70e2f481e7ae0738d6376db728d6
ortizal19009 <ort19009@byui.edu>
2021-10-04 15:58:55 -0500

    Actualizacion de ventas

--- proyectos/sistema-contable/componentes/js/main.js
+++ proyectos/sistema-contable/componentes/js/main.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
 
-    $(".section").html("<button onclick = 'imp()'>Imprimiendo</button>");
 })
-const titulo = ("<h1>Hola mundo</h1>");
-function imp(){
-    $(".section").html(titulo);
-}
