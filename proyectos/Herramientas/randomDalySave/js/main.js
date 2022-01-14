42a262d65cfde3e6de8f5dc10b2bb9a2048853e8
ortizal19009 <ort19009@byui.edu>
2022-01-14 00:48:56 -0500

    Actualización de mensajes

--- proyectos/Herramientas/randomDalySave/js/main.js
+++ proyectos/Herramientas/randomDalySave/js/main.js
@@ -2,7 +2,14 @@ $(document).ready(function(){
     $("#generar").on("click",function(){
         var numRandom = Math.random();
         var saves = (Math.floor(numRandom*(6-1)));
-        $("#respuesta").html(saves);        
+        if(saves != 0){
+            $("#respuesta").html(saves);        
+
+        }
+        else{
+            var mensaje = "TE AMO MI AMOR, pero trate nuevamente. Y dele un besito a su esposito 😍";
+            $("#respuesta").html(mensaje);
+        }
         
     })
 })
